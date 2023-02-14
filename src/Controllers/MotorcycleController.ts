fcee29d3a172b40719cdd545ead8e88128480aec
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-02-14 19:43:17 -0300

    Feat: Motorcycle getAll and getById routes done

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -3,6 +3,7 @@ import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleService from '../Services/MotorcycleService';
 
 const HTTP_CREATED_STATUS = 201;
+const HTTP_OK_STATUS = 200;
 
 export default class MotorcycleController {
   private req: Request;
@@ -17,12 +18,44 @@ export default class MotorcycleController {
     this.service = new MotorcycleService();
   }
 
+  private changeObj(obj: any) {
+    return {
+      id: obj._id,
+      model: obj.model,
+      year: obj.year,
+      color: obj.color,
+      status: obj.status,
+      buyValue: obj.buyValue,
+      category: obj.category,
+      engineCapacity: obj.engineCapacity,
+    };
+  }
+
   public async create() {
     const newMotorcycle: IMotorcycle = { ...this.req.body, status: this.req.body.status || false };
 
     try {
       const createdMotorcycle = await this.service.createNewMotorcycle(newMotorcycle);
       return this.res.status(HTTP_CREATED_STATUS).json(createdMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getAll() {
+    try {
+      const allMotorcycle = await this.service.getAllMotorcycle();
+      return this.res.status(HTTP_OK_STATUS).json(allMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getById() {
+    const { id } = this.req.params;
+    try {
+      const motorcycle = await this.service.getMotorcycleById(id);
+      return this.res.status(HTTP_OK_STATUS).json(this.changeObj(motorcycle));
     } catch (error) {
       this.next(error);
     }
--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,6 +1,7 @@
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
 import Motorcycle from '../Domains/Motorcycle';
+import ErrorMap from '../utils/ErrorMap';
 
 const motorcycleODM = new MotorcycleODM();
 
@@ -15,5 +16,18 @@ export default class MotorcycleService {
   public async createNewMotorcycle(newMotorcycleObj: IMotorcycle): Promise<Motorcycle | null> {
     const newMotorcycle = await motorcycleODM.create(newMotorcycleObj);
     return this.createMotorcycleDomain(newMotorcycle);
+  }
+
+  public async getAllMotorcycle(): Promise<(Motorcycle | null)[]> {
+    const motorcycles = await motorcycleODM.getAll();
+    const motorcyclesArray = motorcycles.map((m) =>
+      this.createMotorcycleDomain(m));
+    return motorcyclesArray;
+  }
+
+  public async getMotorcycleById(id: string): Promise<IMotorcycle | null> {
+    const motorcycle = await motorcycleODM.getById(id);
+    if (!motorcycle) throw new ErrorMap(404, 'Motorcycle not found');
+    return motorcycle;
   }
 }
